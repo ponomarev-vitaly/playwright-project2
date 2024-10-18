f7321d2b0655999f24b3058d77589496a129113a
ponomarev-vitaly <vp211082@gmail.com>
2024-10-18 16:46:44 -0500

    update 6 files

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require("@playwright/test");
 test.describe("Examples", () => {
     test("Testing locators", async ({ page }) => {
         //        
-        //await page.goto("/text-box");
+        await page.goto("/text-box");
         //await page.locator('#userName').type("Test Username");
         //await page.locator('[placeholder="name@example.com"]').type("siteco5432@advitize.com");
         //await page.locator('#currentAddress').type("This is the current address");
