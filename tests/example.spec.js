c6a77746314525e04aff4e3489ca249a749c4c36
ponomarev-vitaly <vp211082@gmail.com>
2024-10-18 00:26:44 -0500

    update 2 files and delete 12 files

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -9,7 +9,7 @@ test.describe("Examples", () => {
         await page.locator('[placeholder="name@example.com"]').type("siteco5432@advitize.com");
         await page.locator('#currentAddress').type("This is the current address");
         await page.locator('#permanentAddress').type("This is the permanent address");
-        await page.locator('#submit).click();   
+        await page.locator('#submit').click();   
         
 
     });
