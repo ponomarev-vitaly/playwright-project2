18eaaa00361cfaa1640bd03757092bb48f46f861
ponomarev-vitaly <vp211082@gmail.com>
2024-10-18 00:09:26 -0500

    test: update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -3,7 +3,7 @@ const { test, expect } = require("@playwright/test");
 test.describe("Examples", () => {
     test("Testing locators", async ({ page }) => {
         //        
-        await page.goto("https://demoqa.com/text-box");
+        await page.goto("/");
         await page.locator('#userName').type("Test Username");
         await page.locator('[placeholder="name@example.com"]').type("test@email.com");
         await page.locator('#currentAddress').type("This is the current address");
