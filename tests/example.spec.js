6779c830751844b3ef535b158d5b066b86ca62ef
ponomarev-vitaly <vp211082@gmail.com>
2024-10-18 00:21:55 -0500

    test: update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,12 +4,13 @@ test.describe("Examples", () => {
     test("Testing locators", async ({ page }) => {
         //        
         await page.goto("/text-box");
+        await page.pause;
         await page.locator('#userName').type("Test Username");
         await page.locator('[placeholder="name@example.com"]').type("siteco5432@advitize.com");
         await page.locator('#currentAddress').type("This is the current address");
         await page.locator('#permanentAddress').type("This is the permanent address");
         await page.locator('button.has-text("Submit")').click();   
-        // await page.pause;
+        
 
     });
 
